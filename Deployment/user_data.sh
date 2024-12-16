c8f4f67071b6361ec30c7c7d4d95a0df840915d3
roee4647 <roee4647@gmail.com>
2024-12-16 23:30:24 +0200

    Update user_data.sh

--- Deployment/user_data.sh
+++ Deployment/user_data.sh
@@ -1,43 +1,66 @@
-#!/bin/bash
-
-
+#! bin/bash
+pwd
+cd home/ec2-user
 #Install git and clone files to the Web Server 
 sudo yum update -y   # For Amazon Linux or CentOS
 sudo yum install -y git
 cd 
 mkdir App
-sudo mkdir .aws
 cd App
-git clone https://github.com/roee4643/MemeGenerator
+
+git clone https://github.com/roee4647/MemeGenerator
 
 cd ..
 
 
+export GEMINI_API_KEY=$HOST_GEMINI_API_KEY
 
-sudo python3 -m ensurepip --upgrade
-sudo /usr/bin/python3 -m pip install --upgrade pip
+
+echo $GEMINI_API_KEY
+
+#Gemini_key=$GEMINI_API_KEY
+
+
+
+echo "export GEMINI_API_KEY='$GEMINI_API_KEY'" >> /home/ec2-user/.bash_profile
+source /home/ec2-user/.bash_profile
+
+
+
+sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo > /dev/null <<EOF
+[mongodb-org-8.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://pgp.mongodb.com/server-8.0.asc
+EOF
+
+sudo yum install -y mongodb-org
+#sudo systemctl start mongod
+
+# Configure MongoDB to bind to 0.0.0.0
+sudo sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mongod.conf
+#sudo systemctl stop mongod
+sudo systemctl start mongod
 
 
 cd home/ec2-user
 
-
+ # Create the .env file in the target directory
+cat <<EOF > /home/ec2-user/App/MemeGenerator/MongoDbMemes/.env
+    ${file("${path.module}/.env")}
+EOF
 
 #set and install docker 
-cd home/ec2-user/App/PokemonApi_MongoDB/PokemonFlask-api
-
-
 sudo yum install docker -y
 sudo service docker start
 
-sudo docker build -t meme-api .
-sudo docker run -p 5000:5000 meme-api
 
+cd /home/ec2-user/App/MemeGenerator/GeminiApiApp
+sudo docker build -t gemini-api-app .
+sudo docker run -e GEMINI_API_KEY="$GEMINI_API_KEY" -p 5000:5000 gemini-api-app
 
-
-
-
-
-
-
-
-
+cd /home/ec2-user/App/MemeGenerator/MongoDbMemes
+sudo docker build -t meme-mongo-app .
+sudo docker run -p 80:5001 -e GEMINI_API_KEY="AIzaSyBab5VArMDK59FsdW9hNgR3_5ASaV_tpJU" meme-mongo-app
