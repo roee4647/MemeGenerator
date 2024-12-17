f72648212c1b287bbae81da9d829cbf637133f26
roee4647 <roee4647@gmail.com>
2024-12-17 12:09:23 +0200

    Update Mongo_Utilities_Class.py

--- MongoDbMemes/Mongo_Utilities_Class.py
+++ MongoDbMemes/Mongo_Utilities_Class.py
@@ -1,5 +1,6 @@
 # pokemon_utilities.py
 from pymongo import MongoClient
+import os
 
 
 
@@ -7,6 +8,12 @@ from pymongo import MongoClient
 class Utilities:
     def __init__(self):
         
+        # Retrieve EC2_PUBLIC_IP from environment variables
+        self.instance_ip = os.getenv('EC2_PUBLIC_IP')
+        if not self.instance_ip:
+            raise ValueError("Environment variable EC2_PUBLIC_IP is not set.")
+        
+        # Connect to MongoDB using the EC2_PUBLIC_IP
         self.client = MongoClient(f"mongodb://{self.instance_ip}:27017/")
         self.db = self.client["mydatabase"]  # Specify the database
         self.collection = self.db["Memes"]# Specify the collection
